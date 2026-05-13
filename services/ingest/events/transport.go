61a46a2cd34642cfdcad2c24ed4ac46b2b0a29a5
Prasant Koirala <koiralaprashanta10@gmail.com>
2026-05-13 13:02:10 -0500

    Feat: add initial event transport structures and main entry point

---
+++ services/ingest/events/transport.go
@@ -0,0 +1,27 @@
+package events
+
+// we will just mirror the structs in the ./common/src/transport.rs here
+
+type AgentTelemetryEvent struct {
+	SchemaVersion    uint16             `json:"schema_version"`
+	AgentID          string             `json:"agent_id"`
+	TenantID         *string            `json:"tenant_id"`
+	Host             HostInfo           `json:"host"`
+	ObservedAtUnixMs int64              `json:"observed_at_unix_ms"`
+	Event            TransportEventKind `json:"event"`
+}
+
+type HostInfo struct {
+	HostName string `json:"host_name"`
+}
+
+type TransportEventKind struct {
+	Type     string   `json:"type"`
+	PID      uint32   `json:"pid,omitempty"`
+	PPID     uint32   `json:"ppid,omitempty"`
+	UID      uint32   `json:"uid,omitempty"`
+	Comm     string   `json:"comm,omitempty"`
+	Filename string   `json:"filename,omitempty"`
+	Argv     []string `json:"argv,omitempty"`
+	Status   string   `json:"status,omitempty"`
+}
