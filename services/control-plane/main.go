2f2e282a18815d26a1686357366a10f87b9b8d57
Prasant Koirala <koiralaprashanta10@gmail.com>
2026-06-10 20:05:50 -0500

    feat: integrated the apis next is the dockerfile and docker compose

--- services/control-plane/main.go
+++ services/control-plane/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/Prashant-koi/lavender/control-plane/internal/api"
+	"github.com/Prashant-koi/lavender/control-plane/internal/store"
 	"github.com/Prashant-koi/lavender/services/platform/env"
 	"github.com/Prashant-koi/lavender/services/platform/postgres"
 	"github.com/Prashant-koi/lavender/services/platform/shutdown"
@@ -22,4 +27,30 @@ func main() {
 		log.Fatalf("failed to connect to postgres: %v", err)
 	}
 	defer db.Close()
+
+	httpAddr := env.Default("HTTP_ADDR", ":8080")
+	alertStore := store.New(db)
+	apiServer := api.NewServer(alertStore)
+
+	server := &http.Server{
+		Addr:              httpAddr,
+		Handler:           apiServer.Handler(),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		log.Printf("control-plane listening on %s", httpAddr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("control-plane http server: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Printf("control-plane http shutdown: %v", err)
+	}
 }
